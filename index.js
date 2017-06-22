2e0bd48c85390eabd77c4c98f432cb62e54f5419
mur7ay <mur7ay@me.com>
2017-06-22 19:47:19 -0400

    Added the backend

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+var config = {
+    apiKey: "AIzaSyAtkPWP3pUiMFAXlWtO1j20UhcnI_1_OpU",
+    authDomain: "muvenew-4e71e.firebaseapp.com",
+    databaseURL: "https://muvenew-4e71e.firebaseio.com",
+    projectId: "muvenew-4e71e",
+    storageBucket: "",
+    messagingSenderId: "182361216763"
+  };
+  firebase.initializeApp(config);
+
 let _pickupAddress = document.getElementById("address1");
 let _dropoffAddress = document.getElementById("address2");
 let _userName = document.getElementById('name');
 let _userPhone = document.getElementById('phone');
 let _pickDateAndTime = document.getElementById('dpt');
+let _numOfItems = document.getElementById('items');
 
-let addy1, addy2, _name, _phone, _pickupTime;
-
+let addy1, addy2, _name, _phone, _pickupTime, _itemCount;
 
 function initialize() {
   var input = document.getElementById('address1');
@@ -22,6 +32,7 @@ document.getElementById('submitBtn').onclick = function() {
   _name = _userName.value;
   _phone = _userPhone.value;
   _pickupTime = _pickDateAndTime.value;
+  _itemCount = _numOfItems.value;
 
   if (addy1 === "") {
     alert("The pickup address is required");
@@ -36,6 +47,17 @@ document.getElementById('submitBtn').onclick = function() {
   } else {
     alert("Thanks for your request! We'll contact you shortly when we're on the way.");
   }
+
+  var firebaseRef = firebase.database().ref();
+  firebaseRef.child("Request").push({
+      pickup: addy1,
+      dropoff: addy2,
+      name: _name,
+      phone: _phone,
+      pickupTime: _pickupTime,
+      itemCount: _itemCount
+  });
+
 }
 
 // Formatting the phone number as the individual types
@@ -78,3 +100,8 @@ function phoneFormat(input) {
   }
   return input;
 }
+
+$('select[class="form-control"]').on('change',function(){
+  var get = $('select option:selected').val();
+  $('.text-center h2 span').html(get+" USD");
+});
